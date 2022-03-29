b92357e3f0f9656258d3e7d7b4800b9fd5f2c2da
Jo Hill <jos.a.hill@gmail.com>
2022-03-28 21:13:02 -0700

    removed unused code

--- public/js/login.js
+++ public/js/login.js
@@ -1,60 +1,37 @@
 const loginFormHandler = async(event) => {
-event.preventDefault();
+    event.preventDefault();
 
-// Collect values from the login form
-const email = document.querySelector('#email-login').value.trim();
-const password = document.querySelector('#password-login').value.trim();
+    // Collect values from the login form
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
 
-if (email && password) {
-    // Send a POST request to the API endpoint
-    const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
-    });
+    if (email && password) {
+        // Send a POST request to the API endpoint
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-    if (response.ok) {
-        // If successful, redirect the browser to the profile page
+        if (response.ok) {
+            // If successful, redirect the browser to the profile page
+            // document.location.replace('/profile');
 
-        console.log(response)
-        let result = await response.json()
-        console.log(result)
-        if (result.user.role_id = 2) {
-            document.location.replace('/profile');
-        } else if (result.user.role_id = 1) {
-            document.location.replace('/admin');
+            console.log(response)
+            let result = await response.json()
+            console.log(result)
+            if (result.user.role_id = 2) {
+                document.location.replace('/profile');
+            } else if (result.user.role_id = 1) {
+                document.location.replace('/admin');
 
-        };
-    } else {
-        alert(response.statusText);
+            };
+        } else {
+            alert(response.statusText);
+        }
     }
-}
 };
 
-}
-}).then(res => {
-if (res.ok) {
-    const userObj = {
-        email: document.querySelector("#email").value,
-        password: document.querySelector("#password").value,
-    }
-    fetch("/api/users/login", {
-        method: "POST",
-        body: JSON.stringify(userObj),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }).then(res => {
-        if (res.ok) {
-            location.href = "/profile"
-        }
-    })
-} else {
-    alert("Yikes!")
-}
-})
-}) *
-/
 
 document
     .querySelector('.login-form')
