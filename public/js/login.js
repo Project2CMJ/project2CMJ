c0918d8abb3547f17e182128c7d7223e599e3fbb
Jo Hill <jos.a.hill@gmail.com>
2022-03-28 21:10:30 -0700

    clean up unnecessary code

--- public/js/login.js
+++ public/js/login.js
@@ -1,101 +1,61 @@
 const loginFormHandler = async(event) => {
-    event.preventDefault();
+event.preventDefault();
 
-    // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+// Collect values from the login form
+const email = document.querySelector('#email-login').value.trim();
+const password = document.querySelector('#password-login').value.trim();
 
-    if (email && password) {
-        // Send a POST request to the API endpoint
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({ email, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
+if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+    });
 
-        if (response.ok) {
-            // If successful, redirect the browser to the profile page
-            // document.location.replace('/profile');
+    if (response.ok) {
+        // If successful, redirect the browser to the profile page
 
-            console.log(response)
-            let result = await response.json()
-            console.log(result)
-            if (result.user.role_id = 2) {
-                document.location.replace('/profile');
-            } else if (result.user.role_id = 1) {
-                document.location.replace('/admin');
+        console.log(response)
+        let result = await response.json()
+        console.log(result)
+        if (result.user.role_id = 2) {
+            document.location.replace('/profile');
+        } else if (result.user.role_id = 1) {
+            document.location.replace('/admin');
 
-            };
-        } else {
-            alert(response.statusText);
-        }
+        };
+    } else {
+        alert(response.statusText);
     }
+}
 };
 
-// const signupFormHandler = async (event) => {
-//   event.preventDefault();
-
-//   const name = document.querySelector('#name-signup').value.trim();
-//   const email = document.querySelector('#email-signup').value.trim();
-//   const password = document.querySelector('#password-signup').value.trim();
-
-//   if (name && email && password) {
-//     const response = await fetch('/api/users', {
-//       method: 'POST',
-//       body: JSON.stringify({ name, email, password }),
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/profile');
-//     } else {
-//       alert(response.statusText);
-//     }
-//   }
-// };
-
-/*
-const signupForm = document.querySelector("#signup-form");
-
-signupForm.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    const userObj={
-        username:document.querySelector("#name-signup").value,
-        email:document.querySelector("#email-signup").value,
-        password:document.querySelector("#password-signup").value,
+}
+}).then(res => {
+if (res.ok) {
+    const userObj = {
+        email: document.querySelector("#email").value,
+        password: document.querySelector("#password").value,
     }
-    fetch("/api/users",{
-        method:"POST",
-        body:JSON.stringify(userObj),
-        headers:{
-            "Content-Type":"application/json"
+    fetch("/api/users/login", {
+        method: "POST",
+        body: JSON.stringify(userObj),
+        headers: {
+            "Content-Type": "application/json"
         }
-    }).then(res=>{
-        if(res.ok){
-            const userObj={
-                email:document.querySelector("#email").value,
-                password:document.querySelector("#password").value,
-            }
-            fetch("/api/users/login",{
-                method:"POST",
-                body:JSON.stringify(userObj),
-                headers:{
-                    "Content-Type":"application/json"
-                }
-            }).then(res=>{
-                if(res.ok){
-                   location.href = "/profile"}})
-        } else {
-            alert("Yikes!")
+    }).then(res => {
+        if (res.ok) {
+            location.href = "/profile"
         }
     })
+} else {
+    alert("Yikes!")
+}
 })
-*/
+}) *
+/
 
 document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
-
-// document
-//     .querySelector('.signup-form')
-//     .addEventListener('submit', signupFormHandler);
